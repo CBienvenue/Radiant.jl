951a7ade93a6422681c925b17e7b6e2fd8f479e9
CBienvenue <charlesbienvenue@live.ca>
2024-04-29 13:45:48 -0400

    Export the UI struct.

--- src/structures/Structures.jl
+++ src/structures/Structures.jl
@@ -38,6 +38,15 @@ include("Fixed_Sources.jl")
 include("Flux_Per_Particle.jl")
 include("Flux.jl")
 include("Computation_Unit.jl")
+export Material
+export Cross_Sections
+export Geometry
+export Discrete_Ordinates
+export Coupled_Transport_Solvers
+export Surface_Source
+export Volume_Source
+export Fixed_Sources
+export Computation_Unit
 
 # To use Python-like notation for methods
 RadiantObject = Union{
