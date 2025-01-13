35095549ccb9da5a35e071ac4a3b38bf50e83947
CBienvenue <charlesbienvenue@live.ca>
2025-01-12 21:57:40 -0500

    Minor correction (too much scattering groups) for custom cross-sections.

--- src/cross_sections/custom_cross_sections.jl
+++ src/cross_sections/custom_cross_sections.jl
@@ -6,7 +6,7 @@ function custom_cross_sections(cross_sections::Cross_Sections)
     for i in range(1,1), n in range(1,Nmat)
         mcs = Multigroup_Cross_Sections(1)
         Σt = zeros(1); Σa = zeros(1); Σs = zeros(1); Σe = zeros(1+1); Σc = zeros(1+1); S = zeros(1+1); α = zeros(1);
-        Σsℓ = zeros(1+1,1,0+1)
+        Σsℓ = zeros(1,1,0+1)
 
         Σt[1] = cross_sections.custom_absorption[n] + cross_sections.custom_scattering[n]
         Σsℓ[1,1,1] = cross_sections.custom_scattering[n]
